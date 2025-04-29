411e3f8e7f78785b113bb5b952b5404a915456a1
alegria1026 <yahirhumberto04@gmail.com>
2025-04-29 05:25:50 -0600

    Header

---
+++ src/app/components/Menu/index.js
@@ -0,0 +1,35 @@
+"use client"
+
+import { useState } from "react"
+import styles from "./Menu.module.css"
+import { IoMdMenu, IoMdClose } from "react-icons/io"
+import Image from "next/image"
+import logo from "./logo.png"
+
+function Menu() {
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  const toggleMenu = () => {
+    setMenuOpen(prev => !prev)
+  }
+
+  return (
+    <nav className={`${styles.navbar} ${menuOpen ? styles.menuOpen : ''}`}>
+      <div className={styles.logo}>
+        <Image src={logo.src} alt="FreshFusion Logo" fill />
+      </div>
+      <button className={styles.menuButton} onClick={toggleMenu}>
+        {menuOpen ? <IoMdClose /> : <IoMdMenu />}
+      </button>
+      <div className={`${styles.navLinks} ${menuOpen ? styles.open : ""}`}>
+        <a href="#" className={styles.navLink}>Inicio</a>
+        <a href="#" className={styles.navLink}>Nosotros</a>
+        <a href="#" className={styles.navLink}>Kits</a>
+        <a href="#" className={styles.navLink}>Sostenibilidad</a>
+        <a href="#" className={styles.navLink}>Contacto</a>
+      </div>
+    </nav>
+  )
+}
+
+export default Menu
---
+++ src/app/pages/Home/index.js
@@ -0,0 +1,14 @@
+import Header from "@/app/components/Header"
+import Menu from "@/app/components/Menu"
+
+
+function Incio() {
+    return (
+        <main>
+            <Menu></Menu>
+            <Header></Header>
+        </main>
+    )
+}
+
+export default Incio
